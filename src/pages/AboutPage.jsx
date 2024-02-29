e2af247704a2a8bcfcc39e40981c183c1ccbd3c3
rnahill <rei333lly@gmail.com>
2024-02-29 13:18:29 -0500

    improved css for about section

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,10 @@
 
 export default function AboutPage() {
-
-<div>
+ return (
+<div className="about-container">
     <h2>About me</h2>
     <img />
-    <p className="container border about-container">
+    <p className="container border border-secondary shadow-lg p-3 m-5 rounded col">
     I am a dedicated student currently enrolled at the University of Pennsylvania, pursuing a certificate in both front and back-end web development. My journey into the world of technology began with a unique path. I began my professional career focusing on pre-physical therapy studies, eventually pivoting towards Veterinary Technology. I earned my Associate's of Veterinary Technology from Manor College, and worked as a veterinary technician before deciding to pursue my passion for tech and coding.
 
     Driven by a thirst for knowledge and a love for creating, I am committed to expanding my expertise beyond web development. My ambitions include learning additional programming languages and exploring the exciting realm of game development. Through my academic and professional experiences, I have cultivated a strong work ethic and honed my ability to collaborate effectively within a team.
@@ -12,6 +12,7 @@ export default function AboutPage() {
     My multifaceted background has equipped me to thrive in fast-paced environments. Coming from a medical background, problem-solving is second nature to me. Whether it's writing lines of code, crafting narratives, or working collaboratively with a team, I bring a versatile skill set and a positive attitude.
     </p>
 </div>
+ );
 
 
 };
