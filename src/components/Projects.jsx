ca9c13c290b950dcd6ededca7053347b4f877486
rnahill <rei333lly@gmail.com>
2024-03-06 10:06:45 -0500

    added images and links to homepage

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,48 @@
+import { Link } from 'react-router-dom';
+import gameHarbour from "../assets/images/gameharbour-image.png"
+import leaflit from "../assets/images/leaflit-image.png"
+import shoppingCart from "../assets/images/shopping-cart.png"
+
+const styles = {
+    img: {
+        maxHeight: "200px"
+        
+    },
+
+    cart: {
+        minHeight: "190px"
+    }
+}
+
 
 export default function Project() {
 
     return (
         <div>
             <section className="row">
-                <div className="border border-secondary col">
-                    <h5>project 1</h5>
+                <div className="border border-secondary col project-card">
+                    <h5>GigHub</h5>
                 </div>
-                <div className="border border-secondary col">
-                    <h5>project 2</h5>
+                <div className="border border-secondary col project-card">
+                    <h5>Game Harbour</h5>
+                    <Link to="https://game-harbour-123-8c07ddc3abc7.herokuapp.com/login">
+                        <img style={styles.img} src={gameHarbour} />
+                    </Link>
                 </div>
             </section>  
 
             <section className="row">
-                <div className="border border-secondary col">
-                    <h5>project 3</h5>
+                <div className="border border-secondary col project-card">
+                    <h5>LeafLit</h5>
+                    <Link to="https://rnahill.github.io/leaflit/">
+                        <img style={styles.img} src={leaflit} />
+                    </Link>
                 </div>
-                <div className="border border-secondary col">
-                    <h5>project 4</h5>
+                <div className="border border-secondary col project-card">
+                    <h5>E-Commerce Backend Application</h5>
+                    <Link to="https://rnahill.github.io/leaflit/">
+                        <img style={styles.cart} src={shoppingCart} />
+                    </Link>
                 </div>
             </section>
         </div>
