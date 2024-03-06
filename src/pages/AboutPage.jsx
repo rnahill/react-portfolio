b1dff8fc3ffb7d9e39efe568babe7b76a4384f4b
rnahill <rei333lly@gmail.com>
2024-03-06 10:37:39 -0500

    changing up classes

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,28 +1,22 @@
 import placeholder from "../assets/images/placeholder.jpg"
 
 const styles = {
-    img: {
-        maxHeight: "200px",
-        maxWidth: "200px",
-        margin: "17%"
-    },
-
-    aboutme: {
-        marginTop: "10%",
-        marginLeft: "17%",
-        maxHeight: "20px"
-    },
+    // img: {
+    //     maxHeight: "200px",
+    //     maxWidth: "200px",
+    //     margin: "17%"
+    // },
 
 };
 
 export default function AboutPage() {
  return (
 <div className="row about-container d-flex align-items-center">
-    <div className="col row">
-        <h2 style={styles.aboutme} className="col">About me</h2>
-        <img style={styles.img} src={placeholder} />
+    <div className="col">
+        <h2 className="col d-flex justify-content-center">About me</h2>
+        <img />   
     </div>
-    <p className="container border border-secondary shadow-lg p-3 m-5 rounded">
+    <p className="col-7 container border border-secondary shadow-lg p-3 m-5 rounded bio-container">
     I am a dedicated student currently enrolled at the University of Pennsylvania, pursuing a certificate in both front and back-end web development. My journey into the world of technology began with a unique path. I began my professional career focusing on pre-physical therapy studies, eventually pivoting towards Veterinary Technology. I earned my Associate's of Veterinary Technology from Manor College, and worked as a veterinary technician before deciding to pursue my passion for tech and coding.
 
     Driven by a thirst for knowledge and a love for creating, I am committed to expanding my expertise beyond web development. My ambitions include learning additional programming languages and exploring the exciting realm of game development. Through my academic and professional experiences, I have cultivated a strong work ethic and honed my ability to collaborate effectively within a team.
