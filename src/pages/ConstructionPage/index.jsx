a15f5c8b8204e9cecfc84a559efa930a12f315d5
rnahill <rei333lly@gmail.com>
2024-04-29 16:24:45 -0400

    added linkedin to construuction page

--- src/pages/ConstructionPage/index.jsx
+++ src/pages/ConstructionPage/index.jsx
@@ -16,9 +16,12 @@ export default function ConstructionPage() {
         <div style={styles.main}>
             <h1>Oops!</h1>
             <h2>This portfolio is currently under construction.</h2>
-            <h3>In the meantime, check out my Github to see my code!</h3>
+            <h3>In the meantime, check out my Github to see my code and my Linkedin to see more about me!</h3>
             <Link to ="https://github.com/rnahill">
                 <h3>rnahill</h3>
+            </Link>
+            <Link to ="https://www.linkedin.com/in/reilly-nahill-10b8b51b0/">
+                <h3>Linkedin</h3>
             </Link>
             <img src={catConstruction} />
         </div>
